b3a02afb2444a149ee4972085d409a321081f24c
Priyanka kukreti <133491434+Exploregithubit@users.noreply.github.com>
2024-08-05 15:07:17 +0530

    Update exception_handling.java

--- exception_handling.java
+++ exception_handling.java
@@ -1,14 +1,1 @@
-class exception_handling
-{
-public static void main(String args[])
-{
-	try
-	{
-		system.out.print("Hello"+" "+1/0);
-	}
-	catch(ArithmeticException e)
-	{
-		System.out.print("World");
-	}
-}
-}
+
